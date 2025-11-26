5c03ef68c08ea3bee85c1399a9842537cc32db87
Czy4201_b <speechlessmatt@qq.com>
2025-11-27 00:20:36 +0800

    release 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.czy4201b.fastfill"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.1"
+        versionCode = 2
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
